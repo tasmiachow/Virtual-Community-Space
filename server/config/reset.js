623a9151e6806b65328979ebb5f72f48256bedad
Tasmia Chowdhury <tasmiachowdhury123@gmail.com>
2025-10-07 17:09:25 -0400

    events + loc tables created

--- server/config/reset.js
+++ server/config/reset.js
@@ -5,7 +5,6 @@ import locations from './locations.js';
 
 const createLocationTable = async () => {
     const createLocationTable =`
-        DROP TABLE IF EXISTS locations;
 
         CREATE TABLE IF NOT EXISTS locations (
             id SERIAL PRIMARY KEY,
@@ -18,7 +17,7 @@ const createLocationTable = async () => {
         console.log("🎉 location table created successfully"); 
     }
     catch(err){
-        console.log('⚠️ error creating arcs table', err);
+        console.log('⚠️ error creating locations table', err);
     }
 };
 
@@ -41,8 +40,65 @@ const seedLocationTable = async () => {
     console.log('🌟 All locations inserted in order successfully');
 }
 
-seedLocationTable();
 
 
 
+const createEventTable = async () =>{
+    const createEventsTable = ` 
 
+        CREATE TABLE IF NOT EXISTS events(
+            event_id SERIAL PRIMARY KEY,
+            artist VARCHAR(255) NOT NULL, 
+            date DATE NOT NULL, 
+            time TIMETZ NOT NULL,
+            timezone TEXT NOT NULL, 
+            image_url TEXT NOT NULL, 
+            venue_id INTEGER NOT NULL,
+            FOREIGN KEY (venue_id) REFERENCES locations(venue_id)
+        );
+        `;
+        try {
+        const res = await pool.query(createEventsTable);
+        console.log("🎉 events table created successfully"); 
+        }
+        catch(err){
+            console.log('⚠️ error creating events table', err);
+        }
+};
+
+
+
+const seedEventsTable = async () =>{
+    await createEventTable();
+    for(const event of events){
+        const insertEvent ={
+            text: 'INSERT INTO events (artist, date, time, timezone, image_url, venue_id) VALUES ($1, $2, $3, $4, $5, $6);', 
+            values: [event.artist, event.date, event.time, event.timezone, event.image_url, event.venue_id]
+        };
+        try { 
+            await pool.query(insertEvent);
+            console.log(`✅ ${event.artist} added successfully`);
+        }
+        catch (err){
+            console.error(`⚠️ error inserting ${event.artist}`, err);
+        }
+    }
+    console.log('🌟 All events inserted in order successfully');
+}
+
+const dropTables = async() =>{
+    const dropTables =  `
+        DROP TABLE IF EXISTS events;  
+        DROP TABLE IF EXISTS locations;
+    ` 
+    try{
+        const res = await pool.query(dropTables);
+        console.log("🎉 tables successfully dropped"); 
+    }
+    catch (err){
+        console.log("error droppeing tables", err); 
+    }
+}
+await dropTables();
+await seedLocationTable(); 
+await seedEventsTable();
